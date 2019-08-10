def5bf73f2569d950fbc6c970730739efd903f98
kanomarin <kanomarin@hotmail.com>
2019-08-10 12:16:15 +0800

    Begin Tutorial
    Begin

---
+++ LiuMingTutorial/Source/LiuMingTutorial/LiuMingTutorialGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LiuMingTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIUMINGTUTORIAL_API ALiuMingTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
